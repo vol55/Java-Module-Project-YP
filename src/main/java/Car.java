6773286745a21b08caa3876cadc1b6a3630e1da6
Igor Avdonin <thathorizon@gmail.com>
2024-12-10 18:52:42 +0500

    Init commit

---
+++ src/main/java/Car.java
@@ -0,0 +1,9 @@
+public class Car {
+    String name;
+    int speed;
+
+    Car(String name, int speed) {
+        this.name = name;
+        this.speed = speed;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,31 @@
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Car> cars = new ArrayList<>();
+        String name;
+        int speed;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.printf("— Введите название машины №%d:\n", i + 1);
+            name = scanner.next();
+
+            while (true) {
+                System.out.printf("— Введите скорость машины #%d\n", i + 1);
+                speed = Integer.parseInt(scanner.next());
+                if (speed >= 0 && speed <= 250) {
+                    break;
+                } else {
+                    System.out.println("— Неправильная скорость");
+                }
+            }
+
+            cars.add(new Car(name, speed));
+        }
+
+        Race race = new Race(cars);
+        System.out.printf("Самая быстрая машина: %s\n", race.getWinner().name);
     }
 }

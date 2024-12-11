992a1f400d407a35cb6ef999b1e6bb37e5805e81
ivan <akpx99@gmail.com>
2024-12-11 15:24:46 +0500

    improved verification logic

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         ArrayList<Car> cars = new ArrayList<>();
         String name;
-        int speed;
+        String speed;
 
         for (int i = 0; i < 3; i++) {
             System.out.printf("— Введите название машины №%d:\n", i + 1);
@@ -14,18 +14,28 @@ public class Main {
 
             while (true) {
                 System.out.printf("— Введите скорость машины #%d\n", i + 1);
-                speed = Integer.parseInt(scanner.next());
-                if (speed >= 0 && speed <= 250) {
+                speed = scanner.next();
+                if (isSpeedLegit(speed)) {
                     break;
-                } else {
-                    System.out.println("— Неправильная скорость");
                 }
+                System.out.println("— Неправильная скорость");
             }
 
-            cars.add(new Car(name, speed));
+            cars.add(new Car(name, Double.parseDouble(speed)));
         }
 
+        scanner.close();
         Race race = new Race(cars);
         System.out.printf("Самая быстрая машина: %s\n", race.getWinner().name);
+    }
+
+    private static boolean isSpeedLegit(String userInput) {
+        try {
+            double speed = Double.parseDouble(userInput);
+            return speed >= 0 && speed <= 250;
+
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
